3aa30c26ea5ed78da05d4bbecffc5fc2120bb5ca
ebdelfin <eugenebdelfin@gmail.com>
2018-01-06 00:04:46 +0800

    Quiz #1

--- Prototype/src/apc/entjava/jkem/keys.java
+++ Prototype/src/apc/entjava/jkem/keys.java
@@ -4,16 +4,35 @@ import javax.faces.bean.ManagedBean;
 
         @ManagedBean
 public class keys {
-    private String option;
+    private String question1;
+    private String question2;
+    private String question3;
 
-            public String getOption() {
-                return option;
+            public String getQuestion1() {
+                return question1;
             }
 
-            public void setOption(String option) {
-                this.option = option;
+            public void setQuestion1(String question1) {
+                this.question1 = question1;
             }
-    public String submit() {
+
+            public String getQuestion2() {
+                return question2;
+            }
+
+            public void setQuestion2(String question2) {
+                this.question2 = question2;
+            }
+
+            public String getQuestion3() {
+                return question3;
+            }
+
+            public void setQuestion3(String question3) {
+                this.question3 = question3;
+            }
+
+            public String submit() {
                 return "missing";
     }
 
