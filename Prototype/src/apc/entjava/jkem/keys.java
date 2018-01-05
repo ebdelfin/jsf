23a4f57511434d759a157e773b43a25d71f0ad20
ebdelfin <eugenebdelfin@gmail.com>
2018-01-05 21:02:28 +0800

    36

---
+++ Prototype/src/apc/entjava/jkem/keys.java
@@ -0,0 +1,16 @@
+package apc.entjava.jkem;
+import javax.faces.bean.ManagedBean;
+
+
+        @ManagedBean
+public class keys {
+    private String option;
+
+            public String getOption() {
+                return option;
+            }
+
+            public void setOption(String option) {
+                this.option = option;
+            }
+        }
